3ccb4bcd259ad6b3ff68dcdfbcb08daab9186448
c-orlando <cleviojr20@gmail.com>
2019-11-02 11:07:48 -0300

    Connect to anilist API and render React app 1) Find token in credentials file 2) Create apollo client with authorization 3) Render Yuiko component inside ApolloProvider

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import './index.css';
+import Yuiko from './screens/Yuiko';
+import * as serviceWorker from './serviceWorker';
+import { token } from './credentials.json';
+
+const client = new ApolloClient({
+  uri: 'https://graphql.anilist.co',
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+  },
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Yuiko />
+  </ApolloProvider>, document.getElementById('root'),
+);
+
+serviceWorker.register();
