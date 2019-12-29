006d8955f305c90ed0c5c0bb54c23c2066ba31a1
c-or <cleviojr20@gmail.com>
2019-12-28 23:03:42 -0300

    [Testing] test components used in Yuiko
    They are temporary, I'm just using it to learn how react works.

---
+++ src/screens/AnimeList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AnimeList() {
+  return <h1>This is an Anime List</h1>;
+}
---
+++ src/screens/NotAnimeList.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotAnimeList() {
+  return <h1>This is not an Anime List</h1>;
+}
