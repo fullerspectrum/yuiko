1dfe69dd11e8fe6d31787a169230d48a90722f55
c-or <cleviojr20@gmail.com>
2020-01-14 01:37:27 -0300

    remove key prop from tds
    it is not needed as they're not a list

--- src/components/ListEntry/ListEntry.jsx
+++ src/components/ListEntry/ListEntry.jsx
@@ -16,18 +16,10 @@ export default function ListEntry({ data }) {
   };
   return (
     <tr>
-      <td id={`title-${data.title}`} key={`title-${data.title}`}>
-        {data.title}
-      </td>
-      <td id={`progress-${data.title}`} key={`progress-${data.title}`}>
-        {`${data.progress}/${data.episodes}`}
-      </td>
-      <td id={`score-${data.title}`} key={`score-${data.title}`}>
-        {data.score === 0 ? '-' : data.score}
-      </td>
-      <td id={`type-${data.title}`} key={`type-${data.title}`}>
-        {data.type}
-      </td>
+      <td id={`title-${data.title}`}>{data.title}</td>
+      <td id={`progress-${data.title}`}>{`${data.progress}/${data.episodes}`}</td>
+      <td id={`score-${data.title}`}>{data.score === 0 ? '-' : data.score}</td>
+      <td id={`type-${data.title}`}>{data.type}</td>
     </tr>
   );
 }
