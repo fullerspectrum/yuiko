47e9dca4b7e8e3d383edb29e97ee2217b1e7d2bb
c-or <cleviojr20@gmail.com>
2020-01-13 22:48:50 -0300

    Add list container and entry
    - For now, it is working as a table
    -- I might in the future use some bootstrap component
    - It contains data other than just the title

---
+++ src/components/ListContainer/ListContainer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Resizer from 'react-column-resizer';
+import PropTypes from 'prop-types';
+import './ListContainer.css';
+
+export default function ListContainer({ children }) {
+  ListContainer.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  };
+  ListContainer.defaultProps = {
+    children: null,
+  };
+
+  return (
+    <table>
+      <tr>
+        <td>Title</td>
+        <td style={{ paddingRight: '40px' }}>Progress</td>
+        <td style={{ paddingRight: '40px' }}>Score</td>
+        <td style={{ paddingRight: '40px' }}>Type</td>
+      </tr>
+      {children}
+    </table>
+  );
+}
--- src/components/ListEntry/ListEntry.jsx
+++ src/components/ListEntry/ListEntry.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function AnimeEntry({ title }) {
+export default function AnimeEntry({ data }) {
+  AnimeEntry.propTypes = {
+    data: PropTypes.shape({
+      title: PropTypes.string,
+      progress: PropTypes.number,
+      score: PropTypes.number,
+      type: PropTypes.string,
+    }),
+  };
+  AnimeEntry.defaultProps = {
+    data: {},
+  };
   return (
-    <div>
-      <p>{title}</p>
-    </div>
+    <tr>
+      <td id={`title-${data.title}`} key={`title-${data.title}`}>
+        {data.title}
+      </td>
+      <td id={`progress-${data.title}`} key={`progress-${data.title}`}>
+        {data.progress}
+      </td>
+      <td id={`score-${data.title}`} key={`score-${data.title}`}>
+        {data.score === 0 ? '-' : data.score}
+      </td>
+      <td id={`type-${data.title}`} key={`type-${data.title}`}>
+        {data.type}
+      </td>
+    </tr>
   );
 }
