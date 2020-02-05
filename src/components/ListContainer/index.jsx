6123af9796f0a45a80446622c419bfe17aef0feb
Clévio Orlando <cleviojr20@gmail.com>
2020-02-05 19:09:41 -0300

    rename all .css files

--- src/components/ListContainer/index.jsx
+++ src/components/ListContainer/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './styles.css';
+import './style.css';
 
 export default function ListContainer({ children }) {
   return (
--- src/screens/List/index.jsx
+++ src/screens/List/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import './styles.css';
+import './style.css';
 import ListContainer from '../../components/ListContainer';
 import ListEntry from '../../components/ListEntry';
 
