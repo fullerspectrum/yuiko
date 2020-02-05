54f9cc068c76b3fe9d4ca4ab9ea57c0f7a55c3c5
Clévio Orlando <cleviojr20@gmail.com>
2020-02-04 21:53:35 -0300

    list/index: refactor conditional rendering

--- src/screens/List/index.jsx
+++ src/screens/List/index.jsx
@@ -5,54 +5,59 @@ import './styles.css';
 import ListContainer from '../../components/ListContainer';
 import ListEntry from '../../components/ListEntry';
 
-// think better name for function, like, listScreen or smth
-export default function list({ lists, params }) {
+export default function List({ lists, params }) {
   const [currentList] = lists.filter((item) => item.name === params.listName);
+
+  let listEntries;
+  let navbarLinks;
+
+  if (lists) {
+    navbarLinks = lists.map((item) => (
+      <Link
+        className={params.listName === item.name ? 'active' : null}
+        id={item.name}
+        key={`navbar-entry-${item.name}`}
+        to={() => `/${params.listType}/${item.name}`}
+      >
+        {item.name}
+      </Link>
+    ));
+  }
+
+  if (currentList) {
+    listEntries = currentList.entries.map((value) => (
+      <ListEntry
+        key={`list-entry-${value.id}`}
+        data={{
+          title: value.media.title.romaji,
+          id: value.id,
+          progress: value.progress,
+          episodes: value.media.episodes,
+          score: value.score,
+          type: value.media.format,
+        }}
+      />
+    ));
+  }
+
   return (
     <div>
-      {lists && (
-        <div className="List-navbar">
-          <nav>
-            {lists.map((item) => (
-              <Link
-                className={params.listName === item.name ? 'active' : null}
-                id={item.name}
-                key={item.name}
-                to={() => `/${params.listType}/${item.name}`}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </nav>
-        </div>
-      )}
-      {currentList && (
-        <div className="List-content">
-          <ListContainer>
-            {currentList.entries.map((value) => (
-              <ListEntry
-                key={value.media.title.romaji}
-                data={{
-                  title: value.media.title.romaji,
-                  progress: value.progress,
-                  episodes: value.media.episodes,
-                  score: value.score,
-                  type: value.media.format,
-                }}
-              />
-            ))}
-          </ListContainer>
-        </div>
-      )}
+      <div className="List-navbar">
+        <nav>{navbarLinks}</nav>
+      </div>
+
+      <div className="List-content">
+        <ListContainer>{listEntries}</ListContainer>
+      </div>
     </div>
   );
 }
 
-list.propTypes = {
+List.propTypes = {
   lists: PropTypes.arrayOf(PropTypes.object),
   params: PropTypes.shape({ listType: PropTypes.string, listName: PropTypes.string }),
 };
-list.defaultProps = {
+List.defaultProps = {
   lists: [],
   params: {},
 };
