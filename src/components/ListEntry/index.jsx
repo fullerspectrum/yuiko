b23502ea88360b02a253140f803b48ad56b73a28
Clévio Orlando <cleviojr20@gmail.com>
2020-02-04 21:57:32 -0300

    ListEntry/index: replace data.title with data.id
    - for better and unique identification

--- src/components/ListEntry/index.jsx
+++ src/components/ListEntry/index.jsx
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 export default function listEntry({ data }) {
   return (
     <tr>
-      <td id={`title-${data.title}`}>{data.title}</td>
-      <td id={`progress-${data.title}`}>{`${data.progress}/${data.episodes}`}</td>
-      <td id={`score-${data.title}`}>{data.score === 0 ? '-' : data.score}</td>
-      <td id={`type-${data.title}`}>{data.type}</td>
+      <td id={`title-${data.id}`}>{data.title}</td>
+      <td id={`progress-${data.id}`}>{`${data.progress}/${data.episodes}`}</td>
+      <td id={`score-${data.id}`}>{data.score === 0 ? '-' : data.score}</td>
+      <td id={`type-${data.id}`}>{data.type}</td>
     </tr>
   );
 }
@@ -15,6 +15,7 @@ export default function listEntry({ data }) {
 listEntry.propTypes = {
   data: PropTypes.shape({
     title: PropTypes.string,
+    id: PropTypes.number,
     progress: PropTypes.number,
     episodes: PropTypes.number,
     score: PropTypes.number,
