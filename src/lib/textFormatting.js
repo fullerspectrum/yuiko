0ffb55d8a8e5736c8c0e6932b3a2e3b46407b08d
clevioorlando <cleviojr20@gmail.com>
2020-07-03 13:31:59 -0300

    textFormatting: format code

--- src/lib/textFormatting.js
+++ src/lib/textFormatting.js
@@ -1,16 +1,12 @@
-function typeFormat(text){
-  let edited = "";
-  if(text.length < 4)
-    return text;
-  else if(text == "TV_SHORT")
-    edited = "TV Short";
-  else if(text == "ONE_SHOT")
-    edited = "One Shot";
-  else
-    edited = text.substr(0,1) + text.substr(1).toLowerCase();
+function typeFormat(text) {
+  let edited = '';
+  if (text.length < 4) return text;
+  if (text === 'TV_SHORT') edited = 'TV Short';
+  else if (text === 'ONE_SHOT') edited = 'One Shot';
+  else edited = text.substr(0, 1) + text.substr(1).toLowerCase();
   return edited;
 }
 
 module.exports = {
-  typeFormat
-}
+  typeFormat,
+};
