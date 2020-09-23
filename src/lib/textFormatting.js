fbf8617aae0e2b1bb810b131e8d7895800e76d4d
Isaiah Fuller <fuller.spectrum@gmail.com>
2020-09-23 12:33:49 -0400

    Fix error on unknown format in lists

--- src/lib/textFormatting.js
+++ src/lib/textFormatting.js
@@ -1,10 +1,14 @@
 function typeFormat(text) {
-  let edited = '';
-  if (text.length < 4) return text;
-  if (text === 'TV_SHORT') edited = 'TV Short';
-  else if (text === 'ONE_SHOT') edited = 'One Shot';
-  else edited = text.substr(0, 1) + text.substr(1).toLowerCase();
-  return edited;
+  let edited = 'Unknown';
+  if(text){
+    if (text.length < 4) return text;
+    if (text === 'TV_SHORT') edited = 'TV Short';
+    else if (text === 'ONE_SHOT') edited = 'One Shot';
+    else edited = text.substr(0, 1) + text.substr(1).toLowerCase();
+    return edited;
+  } else {
+    return edited;
+  }
 }
 
 module.exports = {
